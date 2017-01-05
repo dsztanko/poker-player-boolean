3b9220b66856dfdd2c6b0f6a6ede54a835421df7
Komlancz Janos <jkomlancz@gmail.com>
2017-01-05 13:23:26 +0100

    clean code

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -12,22 +12,12 @@ public class Player {
         try {
             JsonObject jsonObject = request.getAsJsonObject();
             // TODO: work in this
-//            switch (jsonObject.getAsJsonArray("community_cards").size()) {
-//                case 0: {
-//                    return preflop(jsonObject);
-//                }
-//            }
-            if (jsonObject.getAsJsonArray("community_cards").size() == 0) {
-                for (JsonElement team: jsonObject.getAsJsonArray("players")){
-                    JsonObject actualTeam = team.getAsJsonObject();
-                    if (actualTeam.get("name").getAsString().equals("BooLean")) {
-                        JsonArray holeCards = actualTeam.getAsJsonArray("hole_cards");
-                        if (holeCards.get(0).getAsJsonObject().get("rank").getAsString()
-                                .equals(holeCards.get(1).getAsJsonObject().get("rank").getAsString())) {
-                            return actualTeam.get("stack").getAsInt();
-                        }
-                    }
+            switch (jsonObject.getAsJsonArray("community_cards").size()) {
+                case 0: {
+                    return preflop(jsonObject);
                 }
+                case 3:
+                    return afterFlop(jsonObject);
             }
             return 0;
         } catch (Exception e) {
@@ -35,8 +25,34 @@ public class Player {
         }
     }
 
-    private static Integer preflop(JsonObject reqest) {
+    private static int afterFlop(JsonObject jsonObject) {
 
+        return 0;
+    }
+
+    private static boolean checkMiddlePair(JsonObject jsonObject){
+
+        return false;
+    }
+
+    private static boolean checkPairInHand(JsonArray holeCards){
+        if (holeCards.get(0).getAsJsonObject().get("rank").getAsString()
+                .equals(holeCards.get(1).getAsJsonObject().get("rank").getAsString())) {
+            return true;
+        }
+        return false;
+    }
+
+    private static Integer preflop(JsonObject jsonObject) {
+        for (JsonElement team: jsonObject.getAsJsonArray("players")){
+            JsonObject actualTeam = team.getAsJsonObject();
+            if (actualTeam.get("name").getAsString().equals("BooLean")) {
+                JsonArray holeCards = actualTeam.getAsJsonArray("hole_cards");
+                if (checkPairInHand(holeCards)) {
+                    return actualTeam.get("stack").getAsInt();
+                }
+            }
+        }
         return 0;
     }
 
