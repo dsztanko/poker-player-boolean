4086a9f58d2dc85bd9ee8db2f962c7d73dfc4cc2
Komlancz Janos <jkomlancz@gmail.com>
2017-01-05 14:06:58 +0100

    check middle pair done works

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -4,9 +4,23 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import java.util.HashMap;
+
 public class Player {
 
     static final String VERSION = "Default Java folding player";
+//    private static HashMap<String, Integer> cardValues;
+
+//    {
+//        cardValues = new HashMap<>();
+//        cardValues.put("A",14);
+//        cardValues.put("K",13);
+//        cardValues.put("Q",12);
+//        cardValues.put("J",11);
+//        for (int i = 2; i <= 10; i++) {
+//            cardValues.put(String.valueOf(i),i);
+//        }
+//    }
 
     public static int betRequest(JsonElement request) {
         try {
@@ -26,16 +40,44 @@ public class Player {
     }
 
     private static int afterFlop(JsonObject jsonObject) {
-
+        JsonObject actualTeam = ownTeam(jsonObject);
+        if (checkMiddlePair(actualTeam, jsonObject)){
+            return actualTeam.get("stack").getAsInt();
+        }
         return 0;
     }
 
-    private static boolean checkMiddlePair(JsonObject jsonObject){
-
-        return false;
+    private static boolean checkMiddlePair(JsonObject actualTeam, JsonObject jsonObject){
+        HashMap<String, Integer> cardValues = new HashMap<>();
+        cardValues.put("A",14);
+        cardValues.put("K",13);
+        cardValues.put("Q",12);
+        cardValues.put("J",11);
+        for (int i = 2; i <= 10; i++) {
+            cardValues.put(String.valueOf(i),i);
+        }
+        if (checkPairInHand(actualTeam)){
+            System.out.println("chehck middle");
+            int counter = 0;
+            JsonObject holeCard = actualTeam.get("hole_cards").getAsJsonArray().get(0).getAsJsonObject();
+            for (JsonElement communityCard : jsonObject.getAsJsonArray("community_cards")){
+                System.out.println("FOR");
+                System.out.println(cardValues.get("A").toString());
+                System.out.println(cardValues.get(communityCard.getAsJsonObject().get("rank").getAsString()));
+                if(cardValues.get(communityCard.getAsJsonObject().get("rank").getAsString())
+                        > cardValues.get(holeCard.get("rank").getAsString())){
+                    counter++;
+                }
+                if (counter > 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
-    private static boolean checkPairInHand(JsonArray holeCards){
+    private static boolean checkPairInHand(JsonObject actualTeam){
+        JsonArray holeCards = actualTeam.getAsJsonArray("hole_cards");
         if (holeCards.get(0).getAsJsonObject().get("rank").getAsString()
                 .equals(holeCards.get(1).getAsJsonObject().get("rank").getAsString())) {
             return true;
@@ -47,13 +89,22 @@ public class Player {
         for (JsonElement team: jsonObject.getAsJsonArray("players")){
             JsonObject actualTeam = team.getAsJsonObject();
             if (actualTeam.get("name").getAsString().equals("BooLean")) {
-                JsonArray holeCards = actualTeam.getAsJsonArray("hole_cards");
-                if (checkPairInHand(holeCards)) {
+                if (checkPairInHand(actualTeam)) {
                     return actualTeam.get("stack").getAsInt();
                 }
             }
         }
         return 0;
+    }
+
+    private static JsonObject ownTeam(JsonObject jsonObject){
+        for (JsonElement team: jsonObject.getAsJsonArray("players")){
+            JsonObject actualTeam = team.getAsJsonObject();
+            if (actualTeam.get("name").getAsString().equals("BooLean")) {
+                return actualTeam;
+            }
+        }
+        return null;
     }
 
     public static void showdown(JsonElement game) {
--- src/test/java/org/leanpoker/player/PlayerTest.java
+++ src/test/java/org/leanpoker/player/PlayerTest.java
@@ -47,4 +47,42 @@ public class PlayerTest {
         assertEquals(1000, Player.betRequest(jsonElement));
 
     }
+
+    @Test
+    public void testMiddleHandAfterFlop() throws Exception {
+        JsonElement jsonElement = new JsonParser().parse("{\n" +
+                "  \"players\":[\n" +
+                "    {\n" +
+                "      \"name\":\"BooLean\",\n" +
+                "      \"stack\":1000,\n" +
+                "      \"status\":\"active\",\n" +
+                "      \"bet\":0,\n" +
+                "      \"hole_cards\":[{\"rank\":\"K\"},{\"rank\":\"K\"}],\n" +
+                "      \"version\":\"Version name 1\",\n" +
+                "      \"id\":0\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\":\"Player 2\",\n" +
+                "      \"stack\":1000,\n" +
+                "      \"status\":\"active\",\n" +
+                "      \"bet\":0,\n" +
+                "      \"hole_cards\":[],\n" +
+                "      \"version\":\"Version name 2\",\n" +
+                "      \"id\":1\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"tournament_id\":\"550d1d68cd7bd10003000003\",\n" +
+                "  \"game_id\":\"550da1cb2d909006e90004b1\",\n" +
+                "  \"round\":0,\n" +
+                "  \"bet_index\":0,\n" +
+                "  \"small_blind\":10,\n" +
+                "  \"orbits\":0,\n" +
+                "  \"dealer\":0,\n" +
+                "  \"community_cards\":[{\"rank\":\"A\"},{\"rank\":\"J\"},{\"rank\":\"10\"}],\n" +
+                "  \"current_buy_in\":0,\n" +
+                "  \"pot\":0\n" +
+                "}");
+
+        assertEquals(1000, Player.betRequest(jsonElement));
+    }
 }
