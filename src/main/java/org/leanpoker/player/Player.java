3f9ac58c563f14c75629a16e7afea7c233c2385b
adambdnr <adambodnar2@gmail.com>
2017-01-05 13:31:59 +0100

    high cards for preflop

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -12,11 +12,11 @@ public class Player {
         try {
             JsonObject jsonObject = request.getAsJsonObject();
             // TODO: work in this
-//            switch (jsonObject.getAsJsonArray("community_cards").size()) {
-//                case 0: {
-//                    return preflop(jsonObject);
-//                }
-//            }
+            switch (jsonObject.getAsJsonArray("community_cards").size()) {
+                case 0: {
+                    return preflop(jsonObject);
+                }
+            }
             if (jsonObject.getAsJsonArray("community_cards").size() == 0) {
                 for (JsonElement team: jsonObject.getAsJsonArray("players")){
                     JsonObject actualTeam = team.getAsJsonObject();
@@ -37,6 +37,44 @@ public class Player {
 
     private static Integer preflop(JsonObject reqest) {
 
+        return 0;
+    }
+
+    private static Integer highPairPreflop(JsonObject request) {
+        JsonObject jsonObject = request.getAsJsonObject();
+
+
+        for (JsonElement team : jsonObject.getAsJsonArray("players")) {
+            JsonObject actualTeam = team.getAsJsonObject();
+            if (actualTeam.get("name").getAsString().equals("BooLean")) {
+                JsonArray holeCards = actualTeam.getAsJsonArray("hole_cards");
+
+                String card1 = holeCards.get(0).getAsJsonObject().get("rank").getAsString();
+                String card2 = holeCards.get(1).getAsJsonObject().get("rank").getAsString();
+
+
+                switch (card1) {
+                    case "A":
+                        if (card1.equals(card2)) {
+                            return actualTeam.get("stack").getAsInt();
+                        }
+                        return 0;
+
+                    case "K":
+                        if (card1.equals(card2)) {
+                            return actualTeam.get("stack").getAsInt();
+                        }
+                        return 0;
+
+                    case "Q":
+                        if (card1.equals(card2)) {
+                            return actualTeam.get("stack").getAsInt();
+                        }
+                        return 0;
+                }
+            }
+
+        }
         return 0;
     }
 
