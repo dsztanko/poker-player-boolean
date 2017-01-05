832ac0885b5c26e2a596810cd1ee3884fec37fd5
Komlancz Janos <jkomlancz@gmail.com>
2017-01-05 11:40:04 +0100

    done

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,14 +1,28 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-
-import java.util.Map;
+import com.google.gson.JsonObject;
 
 public class Player {
 
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
+
+        JsonObject jsonObject = request.getAsJsonObject();
+        if (jsonObject.getAsJsonArray("community_cards").size() == 0) {
+            for (JsonElement team: jsonObject.getAsJsonArray("players")){
+                JsonObject actualTeam = team.getAsJsonObject();
+                if (actualTeam.get("name").getAsString().equals("BooLean")) {
+                    JsonArray holeCards = actualTeam.getAsJsonArray("hole_cards");
+                    if (holeCards.get(0).getAsJsonObject().get("rank").getAsString().equals("A")
+                            && holeCards.get(1).getAsJsonObject().get("rank").getAsString().equals("A")) {
+                        return actualTeam.get("stack").getAsInt();
+                    }
+                }
+            }
+        }
         return 0;
     }
 
