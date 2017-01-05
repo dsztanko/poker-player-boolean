ef6e5aaa393dbc5bca5a2987ba64b25e0e9e6df9
Ádám Horváth <horada1991@gmail.com>
2017-01-05 11:07:39 +0100

    First commit
    Please work!!

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,9 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import jdk.nashorn.api.scripting.JSObject;
 
 import java.util.Map;
 
@@ -9,7 +12,24 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+        try {
+            JsonObject jsonObject = request.getAsJsonObject();
+            if (jsonObject.getAsJsonArray("community_cards") == null) {
+                for (JsonElement team: jsonObject.getAsJsonArray("players")){
+                    JsonObject actualTeam = team.getAsJsonObject();
+                    if (actualTeam.get("name").equals("BooLean")) {
+                        JsonArray holeCards = actualTeam.getAsJsonArray("hole_cards");
+                        if (holeCards.get(0).getAsJsonObject().get("rank").equals("A")
+                                && holeCards.get(1).getAsJsonObject().get("rank").equals("A")) {
+                            return jsonObject.get("current_buy_in").getAsInt() + jsonObject.get("small_blind").getAsInt();
+                        }
+                    }
+                }
+            }
+            return 0;
+        } catch (Exception e) {
+            return 0;
+        }
     }
 
     public static void showdown(JsonElement game) {
