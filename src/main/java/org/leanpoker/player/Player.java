11122cb1dd764172d6521404f5a7dbd8a3931199
Ádám Horváth <horada1991@gmail.com>
2017-01-05 11:18:54 +0100

    all in on pairs

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -19,9 +19,19 @@ public class Player {
                     JsonObject actualTeam = team.getAsJsonObject();
                     if (actualTeam.get("name").equals("BooLean")) {
                         JsonArray holeCards = actualTeam.getAsJsonArray("hole_cards");
+                       // two A -> allin
+//                        if (holeCards.get(0).getAsJsonObject().get("rank").equals("A")
+//                                && holeCards.get(1).getAsJsonObject().get("rank").equals("A")) {
+//                            return actualTeam.get("stack").getAsInt();
+//                        }
+                        // have a pair -> allin
+                        if (holeCards.get(0).getAsJsonObject().get("rank").equals(holeCards.get(1).getAsJsonObject().get("rank"))) {
+                            return jsonObject.get("current_buy_in").getAsInt() + jsonObject.get("small_blind").getAsInt();
+                        }
+                        // one A -> raise with sb
                         if (holeCards.get(0).getAsJsonObject().get("rank").equals("A")
                                 || holeCards.get(1).getAsJsonObject().get("rank").equals("A")) {
-                            return actualTeam.get("stack").getAsInt();
+                            return jsonObject.get("current_buy_in").getAsInt() + jsonObject.get("small_blind").getAsInt();
                         }
                     }
                 }
